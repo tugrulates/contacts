8135869a374e1310b92c398316f0d9fbd9230002
Tugrul Ates <me@tugrulates.com>
2023-08-07 13:07:38 +0000

    Configure batch size through cli

--- contacts/cli.py
+++ contacts/cli.py
@@ -17,11 +17,12 @@ app = typer.Typer(help=__doc__)
 def find(
     keywords: Annotated[Optional[list[str]], typer.Argument()] = None,
     *,
+    batch: int = 1,
     extend: bool = True,
 ) -> None:
     """List contacts matching given keyword."""
     keywords = keyword.prepare(keywords or [], extend=extend)
-    for person in contact.by_keyword(*keywords):
+    for person in contact.by_keyword(keywords, batch=batch):
         print(
             "{} {}".format(
                 ["  ", ["👤", "🏢"][person.is_company]][person.has_image],
