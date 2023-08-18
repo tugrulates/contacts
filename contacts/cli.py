tugrulates/contacts

commit 2c5d294fd2e634095bbb3c9617cae14a8ef630b2

Option to output as JSON (#52)

contacts/cli.py
@@ -5,7 +5,8 @@ import dataclasses
 from typing import Annotated, Optional
 
 import typer
-from rich import box, print
+from rich import box
+from rich.console import Console
 from rich.progress import Progress
 from rich.table import Table
 
@@ -22,9 +23,9 @@ def with_icon(person: contact.Contact) -> str:
     return f"{person.category.icon} {person.name}"
 
 
-def table(person: contact.Contact, width: Optional[int], safe_box: bool) -> Table:
+def table(person: contact.Contact, width: Optional[int]) -> Table:
     """Create a table view for contact."""
-    table = Table(highlight=True, box=box.ROUNDED, width=width, safe_box=safe_box)
+    table = Table(highlight=True, box=box.ROUNDED, width=width)
     table.add_column(ratio=1, justify="right", style="magenta")
     table.add_column(person.category.icon)
     table.add_column(person.name, ratio=2)
@@ -67,6 +68,7 @@ def find(
     *,
     extend: bool = True,
     detail: bool = False,
+    json: bool = False,
     check: bool = False,
     fix: bool = False,
     applescript: bool = True,
@@ -75,6 +77,7 @@ def find(
     safe_box: bool = True,
 ) -> None:
     """List contacts matching given keyword."""
+    console = Console(width=width, safe_box=safe_box)
     with Progress(transient=True) as progress:
         task = progress.add_task("Counting contacts")
         keywords = keywords or []
@@ -82,21 +85,30 @@ def find(
             keywords = keyword.prepare_keywords(keywords, extend=extend)
 
         address_book = get_address_book(
-            brief=not (detail or check or fix),
+            brief=not (detail or json or check or fix),
             batch=batch or (1 if keywords else 10),
         )
         count = address_book.count(keywords)
         progress.update(task, total=count, description="Fetching contacts")
 
+        people = contact.Contacts()
         for person in address_book.find(keywords):
             if fix:
                 for problem in person.problems:
                     progress.update(task, description=f"Fixing {with_icon(person)}")
                     problem.try_fix(address_book)
                 person = address_book.get(person.contact_id)
-            print(table(person, width, safe_box) if detail else f"{with_icon(person)}")
-            del person.first_name
+
+            if detail:
+                console.print(table(person, width))
+            elif not json:
+                console.print(f"{with_icon(person)}")
+
+            people.contacts.append(person)
             progress.update(task, advance=1, description="Fetching contacts")
+
+        if json:
+            print(people.dumps())
 
 
 if __name__ == "__main__":

contacts/contact.py
@@ -11,6 +11,7 @@ from functools import cached_property
 from pathlib import Path
 from typing import Optional
 
+from pydantic import RootModel
 from pydantic.dataclasses import dataclass
 
 from contacts.category import Category
@@ -202,7 +203,7 @@ class Contact:
         return metadata[0] if metadata else None
 
     @staticmethod
-    def read(path: Path) -> Contact:
+    def load(path: Path) -> Contact:
         """Load contact from json file."""
         with path.open(encoding="utf-8") as file:
             return Contact(**json.load(file))
@@ -210,3 +211,17 @@ class Contact:
     def __str__(self) -> str:
         """Short string for contact."""
         return self.name
+
+
+@dataclass
+class Contacts:
+    """A list of contacts."""
+
+    contacts: list[Contact] = field(default_factory=list)
+
+    def dumps(self) -> str:
+        """Dump JSON string."""
+        return RootModel[Contacts](self).model_dump_json(
+            indent=4,
+            exclude_defaults=True,
+        )

tests/test_cli.py
@@ -1,6 +1,7 @@
 """Unittests for cli."""
 
 
+import json
 from pathlib import Path
 
 import email_validator
@@ -123,8 +124,8 @@ def test_fix_warnings(data_path: Path, mock_address_book: MockAddressBook) -> No
     assert result.stdout.rstrip().split("\n") == [
         "⚠️  dr. warnen bitte sanft jr.",
     ]
-    before = Contact.read(data_path / "warnen.json")
-    after = Contact.read(data_path / "warnen.fixed.json")
+    before = Contact.load(data_path / "warnen.json")
+    after = Contact.load(data_path / "warnen.fixed.json")
     diff = ContactDiff(before, after)
     assert sorted(mock_address_book._updates) == sorted(diff.updates)
     assert sorted(mock_address_book._adds) == sorted(diff.adds)
@@ -141,7 +142,7 @@ def test_errors(mock_address_book: MockAddressBook) -> None:
     ]
 
 
-def test_details_single(data_path: Path, mock_address_book: MockAddressBook) -> None:
+def test_detail_single(data_path: Path, mock_address_book: MockAddressBook) -> None:
     """Test detail with single contact."""
     mock_address_book.provide("amelie")
     result = runner.invoke(cli.app, "--detail --width=79 --no-safe-box")
@@ -150,7 +151,7 @@ def test_details_single(data_path: Path, mock_address_book: MockAddressBook) -> 
     assert result.stdout.strip() == expected_output
 
 
-def test_details_multiple(data_path: Path, mock_address_book: MockAddressBook) -> None:
+def test_detail_multiple(data_path: Path, mock_address_book: MockAddressBook) -> None:
     """Test detail with multiple contacts."""
     mock_address_book.provide("amelie", "bob", "carnival")
     result = runner.invoke(cli.app, "--detail --width=79 --no-safe-box")
@@ -162,7 +163,7 @@ def test_details_multiple(data_path: Path, mock_address_book: MockAddressBook) -
     assert result.stdout.strip() == expected_output
 
 
-def test_details_warnings(data_path: Path, mock_address_book: MockAddressBook) -> None:
+def test_detail_warnings(data_path: Path, mock_address_book: MockAddressBook) -> None:
     """Test warnings on detail."""
     mock_address_book.provide("warnen")
     result = runner.invoke(cli.app, "warnen --detail --width=79 --no-safe-box")
@@ -171,10 +172,23 @@ def test_details_warnings(data_path: Path, mock_address_book: MockAddressBook) -
     assert result.stdout.strip() == expected_output
 
 
-def test_details_errors(data_path: Path, mock_address_book: MockAddressBook) -> None:
+def test_detail_errors(data_path: Path, mock_address_book: MockAddressBook) -> None:
     """Test errors on detail."""
     mock_address_book.provide("errona")
     result = runner.invoke(cli.app, "errona --detail --width=79 --no-safe-box")
     assert result.exit_code == 0
     expected_output = (data_path / "errona.detail").read_text(encoding="utf-8").strip()
     assert result.stdout.strip() == expected_output
+
+
+def test_json(data_path: Path, mock_address_book: MockAddressBook) -> None:
+    """Test detail with multiple contacts."""
+    mock_address_book.provide("amelie", "bob", "carnival")
+    result = runner.invoke(cli.app, "--json --width=1000")
+    assert result.exit_code == 0
+    contacts = [
+        json.loads(Path(data_path / x).read_text(encoding="utf-8"))
+        for x in ["amelie.json", "bob.json", "carnival.json"]
+    ]
+    expected = json.dumps({"contacts": contacts}, indent=4, ensure_ascii=False)
+    assert result.stdout.strip() == expected

commit f11280bf7fc9a99d47adb305f9f48655254b0b6c

Fix progress console line feed
Broken with #52

contacts/cli.py
@@ -78,7 +78,7 @@ def find(
 ) -> None:
     """List contacts matching given keyword."""
     console = Console(width=width, safe_box=safe_box)
-    with Progress(transient=True) as progress:
+    with Progress(transient=True, console=console) as progress:
         task = progress.add_task("Counting contacts")
         keywords = keywords or []
         if applescript: